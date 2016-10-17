79c92fa51565cbc50a4407c6a5fb1e471b7db91d
Egor Suvorov <egor.suvorov@gmail.com>
2016-10-17 22:16:35 +0300

    Add tp.sh and parse.py for testing PrettyPrinter

---
+++ parse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from yat.parser import Parser, Scanner
+from yat.model import Scope
+
+if __name__ == "__main__":
+    import sys
+    for filename in sys.argv[1:]:
+        program = Parser().parse(Scanner(filename))
---
+++ tp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -u
+source ./tlib.sh
+
+download model.py
+download printer.py
+
+for prog in tests/format/*.y; do
+  echo "===== New test ====="
+  function go() {
+    cat $prog
+    echo "====="
+    ./format.py $prog >student.y || return 1
+    ./parse.py student.y || return 1
+    cat student.y
+    echo -n "    Good or bad (g/b)? "
+    read ans
+    [[ "$ans" == "g" ]]
+  }
+  tst "$prog" go
+  rm student.y
+done
+
+validate
