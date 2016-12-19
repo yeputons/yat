94b9a71da8d5d64c0839112dc40929202a0567aa
Egor Suvorov <egor.suvorov@gmail.com>
2016-12-20 01:41:54 +0300

    test_static_analyze.py: add a lot of tests for NoReturnValueCheckVisitor

--- test_static_analyze.py
+++ test_static_analyze.py
@@ -117,8 +117,18 @@ def nrcv():
         return result
     return visit_wrapped
 
+
+@pytest.fixture
+def nrcv_good(nrcv):
+    def visit_wrapped(tree):
+        res = nrcv(FunctionDefinition("foo", Function([], [tree])))
+        assert res == set() or res == set(["foo"])
+        return "foo" not in res
+    return visit_wrapped
+
+
 class TestNoReturnValueCheckVisitor:
-    def test_smoke(self, monkeypatch, nrcv):
+    def test_smoke(self, nrcv):
         prog = (
             Conditional(Number(1), [
                 FunctionDefinition("foo", Function([], [
@@ -141,6 +151,182 @@ class TestNoReturnValueCheckVisitor:
             ])
         )
         assert nrcv(prog) == set(["foo", "baz"])
+
+    class TestNumber:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(Number(1))
+
+    class TestFunction:
+        def test_good(self, nrcv):
+            prog = FunctionDefinition("foo", Function([], [Number(10)]))
+            assert nrcv(prog) == set()
+
+        def test_good_with_args(self, nrcv):
+            prog = FunctionDefinition("foo", Function(["arg"], [Number(10)]))
+            assert nrcv(prog) == set()
+
+        def test_bad_empty(self, nrcv):
+            prog = FunctionDefinition("foo", Function([], []))
+            assert nrcv(prog) == set(["foo"])
+
+        def test_bad_with_args(self, nrcv):
+            prog = FunctionDefinition("foo", Function(["arg"], []))
+            assert nrcv(prog) == set(["foo"])
+
+        def test_bad_not_last(self, nrcv):
+            prog = FunctionDefinition("foo", Function(["arg"], [
+                Conditional(Number(0), [], []),
+                Number(1)
+            ]))
+            assert nrcv(prog) == set()
+
+        def test_definition_containing_good(self, nrcv_good):
+            assert nrcv_good(FunctionDefinition("foo", Function([], [Number(10)])))
+
+        def test_definition_containing_bad(self, nrcv_good):
+            assert not nrcv_good(FunctionDefinition("foo", Function([], [])))
+
+        def test_embedded(self, nrcv):
+            prog = FunctionDefinition("foo1", Function([], [
+                FunctionDefinition("foo2", Function([], [
+                    FunctionDefinition("foo3", Function([], [])),
+                ])),
+                FunctionDefinition("foo4", Function([], [
+                    FunctionDefinition("foo5", Function([], [])),
+                    Conditional(Number(0), [], [])
+                ])),
+                FunctionDefinition("foo6", Function([], []))
+            ]))
+            assert nrcv(prog) == set(["foo3", "foo4", "foo5", "foo6"])
+
+    class TestConditional:
+        def test_none(self, nrcv_good):
+            assert not nrcv_good(Conditional(Number(0), None, None))
+
+        def test_empty(self, nrcv_good):
+            assert not nrcv_good(Conditional(Number(0), [], []))
+
+        def test_empty_false(self, nrcv_good):
+            assert not nrcv_good(Conditional(Number(0), [Number(1)], []))
+
+        def test_none_false(self, nrcv_good):
+            assert not nrcv_good(Conditional(Number(0), [Number(1)], None))
+
+        def test_empty_true(self, nrcv_good):
+            assert not nrcv_good(Conditional(Number(0), [], [Number(1)]))
+
+        def test_none_true(self, nrcv_good):
+            assert not nrcv_good(Conditional(Number(0), None, [Number(1)]))
+
+        def test_non_empty(self, nrcv_good):
+            assert nrcv_good(Conditional(Number(0), [Number(1)], [Number(2)]))
+
+        def test_bad_true(self, nrcv_good):
+            bad = Conditional(Number(0), [], [])
+            assert not nrcv_good(Conditional(Number(0), [bad], [Number(2)]))
+
+        def test_bad_false(self, nrcv_good):
+            bad = Conditional(Number(0), [], [])
+            assert not nrcv_good(Conditional(Number(0), [Number(1)], [bad]))
+
+        def test_bad_not_last(self, nrcv_good):
+            bad = Conditional(Number(0), [], [])
+            assert nrcv_good(Conditional(Number(0), [bad, Number(1)], [bad, Number(2)]))
+
+        def test_embedded(self, nrcv):
+            prog = Conditional(Number(0), [
+                Conditional(Number(0), [], []),
+                FunctionDefinition("foo1", Function([], [])),
+                FunctionDefinition("foo2", Function([], [Number(1)])),
+                Number(2),
+            ], [
+                Conditional(Number(0), [], []),
+                FunctionDefinition("foo3", Function([], [])),
+                FunctionDefinition("foo4", Function([], [Number(3)])),
+                Number(4),
+            ])
+            assert nrcv(prog) == set(["foo1", "foo3"])
+
+        def test_embedded_in_bad_if(self, nrcv):
+            prog = Conditional(Number(0), [
+                FunctionDefinition("foo1", Function([], [])),
+                FunctionDefinition("foo2", Function([], [Number(1)])),
+                Conditional(Number(0), [], [])
+            ], [
+                FunctionDefinition("foo3", Function([], [])),
+                FunctionDefinition("foo4", Function([], [Number(3)])),
+                Conditional(Number(0), [], [])
+            ])
+            assert nrcv(prog) == set(["foo1", "foo3"])
+
+    class TestPrint:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(Print(Number(10)))
+
+        def test_bad_with_cond(self, nrcv_good):
+            assert not nrcv_good(Print(Conditional(Number(0), [], [])))
+
+    class TestRead:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(Read("foo"))
+
+    class TestFunctionCall:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(FunctionCall(Reference("hello"), [Number(1), Number(2), Number(3)]))
+
+        def test_bad_with_cond(self, nrcv_good):
+            assert not nrcv_good(FunctionCall(Reference("hello"), [Number(1), Conditional(Number(0), [], []), Number(3)]))
+
+        def test_embed(self, nrcv):
+            bad = Conditional(Number(0), [], [])
+            def func_def(name):
+                return FunctionDefinition(name, Function([], []))
+            prog = FunctionCall(bad, [Number(1), func_def("foo1"), Number(2), func_def("foo2"), Number(3)])
+            assert nrcv(prog) == set(["foo1", "foo2"])
+
+    class TestReference:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(Reference("foo"))
+
+    class TestBinaryOperation:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(BinaryOperation(Number(1), "+", Number(2)))
+
+        def test_bad_lhs_with_cond(self, nrcv_good):
+            assert not nrcv_good(BinaryOperation(Conditional(Number(0), [], []), "+", Number(2)))
+
+        def test_bad_rhs_with_cond(self, nrcv_good):
+            assert not nrcv_good(BinaryOperation(Number(1), "+", Conditional(Number(0), [], [])))
+
+        def test_bad_both_with_cond(self, nrcv_good):
+            assert not nrcv_good(BinaryOperation(Conditional(Number(0), [], []), "+", Conditional(Number(0), [], [])))
+
+        def test_embedded(self, nrcv):
+            def func_def(name):
+                return FunctionDefinition(name, Function([], []))
+            prog = BinaryOperation(func_def("foo1"), "+", func_def("foo2"))
+            assert nrcv(prog) == set(["foo1", "foo2"])
+
+        def test_embedded_and_bad_lhs(self, nrcv):
+            bad = Conditional(Number(0), [], [])
+            def func_def(name):
+                return FunctionDefinition(name, Function([], []))
+            prog = BinaryOperation(bad, "+", func_def("foo2"))
+            assert nrcv(prog) == set(["foo2"])
+
+        def test_embedded_and_bad_rhs(self, nrcv):
+            bad = Conditional(Number(0), [], [])
+            def func_def(name):
+                return FunctionDefinition(name, Function([], []))
+            prog = BinaryOperation(func_def("foo1"), "+", bad)
+            assert nrcv(prog) == set(["foo1"])
+
+    class TestUnaryOperation:
+        def test_good(self, nrcv_good):
+            assert nrcv_good(UnaryOperation("-", Number(1)))
+
+        def test_bad_expr_with_cond(self, nrcv_good):
+            assert not nrcv_good(UnaryOperation("-", Conditional(Number(0), [], [])))
 
 if __name__ == "__main__":
     pytest.main([sys.argv[0]])
