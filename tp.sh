8d8e66057ed817ea488989d196d902526dc4db99
Egor Suvorov <egor.suvorov@gmail.com>
2017-10-22 17:15:04 +0300

    tp.sh: detect indent of student and unify it with test

--- tp.sh
+++ tp.sh
@@ -13,8 +13,21 @@ for prog in tests/format/*.y; do
     cat $prog
     echo "====="
     ./format.py $prog >student.y || return 1
+
+    sed -i "s/\t/    /g" student.y
+    indent=""
+    for ((i=1; $i <= 10; i++)); do
+      indent+=" "
+      if grep "^$indent[^ ]" student.y; then
+        echo "Detected indent = $indent"
+        sed -i "s/$indent/    /g" student.y
+        break
+      fi
+    done
+    unix2dos student.y >/dev/null 2>&1
+
     ./parse.py student.y || return 1
-    cat student.y
+    diff -y $prog student.y
     echo -n "    Good or bad (g/b)? "
     read ans
     [[ "$ans" == "g" ]]
